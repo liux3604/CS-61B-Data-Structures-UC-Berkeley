ed0418c29a82d5ea4a72fd713d5e3b7e9d8a0a27
Michael Liu <liux3604@gmail.com>
2017-07-13 17:33:13 -0500

    added discussion 7 answer

--- discussion 7/src/Main.java
+++ discussion 7/src/Main.java
@@ -1,11 +1,12 @@
+import java.util.HashMap;
+import java.util.Set;
+
 public class Main {
 
     public static void main(String[] args) {
 
         // Given an integer x and a sorted array A[] of N distinct integers, design an algorithm to find if there
         // exists indices i and j such that A[i] + A[j] == x.
-
-
 
     }
 
@@ -22,5 +23,31 @@ public class Main {
         return false;
     }
 
+    public static int[] overlappingArray(int[] A, int[] B){
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int k: A){
+            if(map.get(k)!=null){
+                map.put(k,map.get(k)+1);
+            }
+        }
+        for(int k: B){
+            if(map.get(k)!=null){
+                map.put(k,map.get(k)+1);
+            }
+        }
+        Set<Integer> keySet = map.keySet();
+        int size= keySet.size();
+        int[] newArray = new int[size];
+        int i=0;
+        for(int key: keySet){
+            if(map.get(key)>=2){
+                newArray[i]=key;
+                i++;
+            }
+        }
+
+        return newArray;
+
+    }
 
 }
